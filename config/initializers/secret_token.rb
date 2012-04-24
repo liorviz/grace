e4945cf2f613e39a50d2a13b071ed0d89d03f85c
L V <liorviz@gmail.com>
2012-04-24 09:30:11 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grace::Application.config.secret_token = '6086c98070a76182d4c34c5d1961a6a564ec0c74edece17218525132c4b871cb5d1b0a1c29468d190f7bb35fc526f79cf7d7a2cafbee462980754ac93947f7b8'
